9950f5aeee7f7e6da3d228513851953f7d8d09fa
Lakhan <166634164+Lakhan815@users.noreply.github.com>
2026-05-29 21:36:32 -0400

    4. working on routing spotify to the db

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
 //go to http://192.168.1.217:3000/api/auth/signin to see the login page
 
+import prisma from "@/lib/prisma";
 import NextAuth from "next-auth";
 import SpotifyProvider from "next-auth/providers/spotify";
 
@@ -11,6 +12,17 @@ const handler = NextAuth({
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
     }),
   ],
+
+  callbacks: {
+    async signIn({ user }) {
+      await prisma.user.upsert({
+        where: { id: user.id },
+        update: { name: user.name || "", email: user.email || "" },
+        create: { id: user.id, name: user.name || "", email: user.email || "" },
+      });
+      return true;
+    },
+  },
 });
 
 //NextAuth needs the GET and POST handlers to function which is why we export them
---
+++ src/lib/prisma.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@prisma/client";
+
+// its not good to do it like this becaues nextjs restarts a lot and this could cause a lot of database calls
+// const prisma = new PrismaClient();
+// export default prisma;
+
+//globals persist through reloads, this is how you cast as global in TS
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+//"If a prisma instance alr exists, use that one otherwise make a new one" || means or
+const prisma = globalForPrisma.prisma || new PrismaClient();
+
+//Save the prisma instance to the global object so you can reuse it next cycle
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+export default prisma;
