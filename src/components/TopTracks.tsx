19f2daf51300107ea4ef55d5744f8c2d5859c926
Lakhan <166634164+Lakhan815@users.noreply.github.com>
2026-06-02 21:49:36 -0400

    fixed up some css

--- src/components/TopTracks.tsx
+++ src/components/TopTracks.tsx
@@ -22,12 +22,12 @@ export default function TopTracks({ tracks }: Props) {
   const topTracks = tracks.map((track) => (
     <li key={track.id} className="flex flex-col p-2 rounded-lg">
       <img src={track.album.images[0].url} alt={track.album.name} />
-      {track.name}
-      {track.artists[0].name}
+      <p>{track.name}</p>
+      <p>{track.artists.map((a) => a.name).join(", ")}</p>
     </li>
   ));
   return (
-    <ul className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-4">
+    <ul className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-4 rounded-lg border-white-30/80 w-full md:w-1/2">
       {topTracks}
     </ul>
   );
