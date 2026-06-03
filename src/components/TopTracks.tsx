19bced342ad1a6ff1b44c59f31f272c8da3ed5d4
Lakhan <166634164+Lakhan815@users.noreply.github.com>
2026-06-02 21:54:04 -0400

    more css

--- src/components/TopTracks.tsx
+++ src/components/TopTracks.tsx
@@ -20,15 +20,20 @@ interface Props {
 
 export default function TopTracks({ tracks }: Props) {
   const topTracks = tracks.map((track) => (
-    <li key={track.id} className="flex flex-col p-2 rounded-lg">
+    <li
+      key={track.id}
+      className="flex flex-col p-2 rounded-lgborder border-white/20"
+    >
       <img src={track.album.images[0].url} alt={track.album.name} />
       <p>{track.name}</p>
       <p>{track.artists.map((a) => a.name).join(", ")}</p>
     </li>
   ));
   return (
-    <ul className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-4 rounded-lg border-white-30/80 w-full md:w-1/2">
-      {topTracks}
-    </ul>
+    <div className="flex justify-center px-8 pt-8 w-full md:w-1/2">
+      <ul className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-4 rounded-lg">
+        {topTracks}
+      </ul>
+    </div>
   );
 }
