9d254a34a1791f233e315bb6ac768ccaf8690240
Lakhan <166634164+Lakhan815@users.noreply.github.com>
2026-06-02 21:36:12 -0400

    13. made the css on the dashboard look a lot nicer

--- src/components/TopTracks.tsx
+++ src/components/TopTracks.tsx
@@ -20,11 +20,15 @@ interface Props {
 
 export default function TopTracks({ tracks }: Props) {
   const topTracks = tracks.map((track) => (
-    <li key={track.id}>
+    <li key={track.id} className="flex flex-col p-2 rounded-lg">
+      <img src={track.album.images[0].url} alt={track.album.name} />
       {track.name}
       {track.artists[0].name}
-      <img src={track.album.images[0].url} alt={track.album.name} />
     </li>
   ));
-  return <ul>{topTracks}</ul>;
+  return (
+    <ul className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-4">
+      {topTracks}
+    </ul>
+  );
 }
