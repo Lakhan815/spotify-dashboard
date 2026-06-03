9c9245e0e6b1dbb3d82415dd61c390ae2c032803
Lakhan <166634164+Lakhan815@users.noreply.github.com>
2026-06-02 22:27:56 -0400

    more fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 "use client";
 import Image from "next/image";
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
   const { data: session } = useSession();
+  const router = useRouter();
+
+  if (session) {
+    router.push("/dashboard");
+  }
+
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
