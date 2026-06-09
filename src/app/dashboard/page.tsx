352ba4622480b16378fc8e9c1cf66e0703dfe870
Lakhan <166634164+Lakhan815@users.noreply.github.com>
2026-06-09 18:01:56 -0400

    17. added a genre chart

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,13 +3,16 @@ export const dynamic = "force-dynamic";
 import { useEffect, useState } from "react";
 import TopTracks from "@/components/TopTracks";
 import TopArtists from "@/components/TopArtists";
+import GenreChart from "@/components/GenreChart";
 
 export default function Dashboard() {
   const [tracks, setTracks] = useState<any[]>([]);
   const [artists, setArtists] = useState<any[]>([]);
   const [timeRange, setTimeRange] = useState("short_term");
   const [selection, setSelection] = useState("tracks");
-
+  const [genreData, setGenreData] = useState<{ name: string; count: number }[]>(
+    [],
+  );
   useEffect(() => {
     if (selection != "tracks") return;
     async function fetchData() {
@@ -34,8 +37,35 @@ export default function Dashboard() {
     fetchData();
   }, [timeRange, selection]);
 
+  useEffect(() => {
+    async function fetchGenres() {
+      const response = await fetch(
+        "/api/spotify/top-artists?range=" + timeRange,
+      );
+      const result = await response.json();
+      const genreMap = new Map();
+      for (let i = 0; i < result.items.length; i++) {
+        for (let j = 0; j < result.items[i].genres.length; j++) {
+          if (!genreMap.has(result.items[i].genres[j])) {
+            genreMap.set(result.items[i].genres[j], { val: 1 });
+          } else {
+            genreMap.get(result.items[i].genres[j]).val++;
+          }
+        }
+      }
+      const genreArray = Array.from(genreMap, ([name, val]) => ({
+        name,
+        count: val.val,
+      }))
+        .sort((a, b) => b.count - a.count)
+        .slice(0, 10);
+      setGenreData(genreArray);
+    }
+    fetchGenres();
+  }, [timeRange]);
+
   const btnClass =
-    "relative inline-flex items-center p-0.5 overflow-hidden text-sm font-medium rounded-lg group bg-gradient-to-br from-teal-300 to-[#363636] hover:from-teal-400 hover:to-[#444]";
+    "relative inline-flex items-center p-0.5 overflow-hidden text-sm font-medium rounded-lg group bg-gradient-to-br from-lime-300 to-[#363636] hover:from-lime-400 hover:to-[#444]";
   const spanClass =
     "relative px-4 py-2 bg-black rounded-lg text-white group-hover:bg-transparent transition-all duration-75";
 
@@ -74,6 +104,7 @@ export default function Dashboard() {
       <hr className="border-white/10 mb-6" />
       {selection === "tracks" && <TopTracks tracks={tracks} />}
       {selection === "artists" && <TopArtists artists={artists} />}
+      <GenreChart genreData={genreData} />
     </div>
   );
 }
---
+++ src/components/GenreChart.tsx
@@ -0,0 +1,41 @@
+import {
+  BarChart,
+  Bar,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+  Legend,
+} from "recharts";
+
+interface GenreData {
+  name: string;
+  count: number;
+}
+
+interface Props {
+  genreData: GenreData[];
+}
+
+export default function GenreChart({ genreData }: Props) {
+  return (
+    <ResponsiveContainer width="100%" height={400}>
+      <BarChart
+        style={{
+          width: "100%",
+          maxWidth: "700px",
+          maxHeight: "70vh",
+          aspectRatio: 1.618,
+        }}
+        data={genreData}
+        margin={{ top: 5, right: 0, left: 0, bottom: 5 }}
+      >
+        <XAxis dataKey="name" />
+        <YAxis width="auto" />
+        <Tooltip />
+        <Legend />
+        <Bar fill="#82ca9d" radius={[10, 10, 0, 0]} dataKey="count" />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+}
