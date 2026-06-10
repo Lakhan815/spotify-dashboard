5657e02be3b4736ee95cc9a29b62c12725ce3732
Lakhan <166634164+Lakhan815@users.noreply.github.com>
2026-06-09 22:09:34 -0400

    more fixes

--- src/components/DurationChart.tsx
+++ src/components/DurationChart.tsx
@@ -30,7 +30,12 @@ export default function DurationChart({ durationData }: Props) {
         data={durationData}
         margin={{ top: 5, right: 0, left: 0, bottom: 5 }}
       >
-        <XAxis dataKey="name" />
+        <XAxis
+          dataKey="name"
+          stroke="#ffffff"
+          tick={{ fill: "#ffffff", fontSize: 10 }}
+        />
+        <YAxis stroke="#ffffff" tick={{ fill: "#ffffff" }} />
         <Tooltip />
         <Legend />
         <Bar fill="#82ca9d" radius={[10, 10, 0, 0]} dataKey="duration" />
