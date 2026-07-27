2482fa810fb492681f91ea96ba7f4d5470869ef6
Lakhan <166634164+Lakhan815@users.noreply.github.com>
2026-07-27 15:07:52 -0400

    more css added framer

--- src/components/TopTracks.tsx
+++ src/components/TopTracks.tsx
@@ -1,3 +1,4 @@
+import { motion } from "motion/react";
 //sets up the interfaces:
 //this interface reflects the format the data is outputted from the spotify API
 interface Track {
@@ -21,23 +22,39 @@ interface Props {
   tracks: Track[];
 }
 
+const container = {
+  hidden: {},
+  show: {
+    transition: { staggerChildren: 0.08 },
+  },
+};
+
+const item = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1 },
+};
+
 //actually displaying the tracks
 export default function TopTracks({ tracks }: Props) {
   const topTracks = tracks.map((track) => (
-    <li
+    <motion.li
       key={track.id}
       className="flex flex-col p-2 rounded-lg border border-white/50"
     >
       <img src={track.album.images?.[0]?.url} alt={track.album.name} />{" "}
       <p>{track.name}</p>
       <p>{track.artists.map((a) => a.name).join(", ")}</p>
-    </li>
+    </motion.li>
   ));
   return (
     <div className="flex justify-center px-8 pt-8 ">
-      <ul className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-4 rounded-lg">
+      <motion.ul
+        className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-4 rounded-lg"
+        initial="hidden"
+        animate="show"
+      >
         {topTracks}
-      </ul>
+      </motion.ul>
     </div>
   );
 }
